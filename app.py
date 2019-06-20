acb86518199a0c43eaecb24be493e53c6ea3267b
HMS <harrisonsbennett@gmail.com>
2019-06-20 11:54:28 +0100

    Updated server to run index from subfolder

---
+++ app.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8088
+
+web_dir = os.path.join(os.path.dirname(__file__), 'public')
+os.chdir(web_dir)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+	print("serving at port", PORT)
+	httpd.serve_forever()
+
+
